d27d9ae83ab3da8b94c825c4e88c4b9b0e161627
Administrator <administrator@che-lt-i41644lx.microchip.com>
2026-08-10 16:32:32 +0530

    Added stremer.service , integrated camera and PIR sensor together

--- src/start_stream.sh
+++ src/start_stream.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 
+#Static IP for eth0 interface
+/sbin/ifconfig eth0 192.168.10.18 netmask 255.255.255.0 up
+
 #Configuring Media controller pipeline
   
-media-ctl -d /dev/media0 --set-v4l2 '"imx219 1-0010":0[fmt:SRGGB10_1X10/640x480]'
-media-ctl -d /dev/media0 --set-v4l2 '"dw-csi.0":0[fmt:SRGGB10_1X10/640x480]'
-media-ctl -d /dev/media0 --set-v4l2 '"csi2dc":0[fmt:SRGGB10_1X10/640x480]'
-media-ctl -d /dev/media0 --set-v4l2 '"microchip_isc_scaler":0[fmt:SRGGB10_1X10/640x480]'
+/usr/bin/media-ctl -d /dev/media0 --set-v4l2 '"imx219 1-0010":0[fmt:SRGGB10_1X10/640x480]'
+/usr/bin/media-ctl -d /dev/media0 --set-v4l2 '"dw-csi.0":0[fmt:SRGGB10_1X10/640x480]'
+/usr/bin/media-ctl -d /dev/media0 --set-v4l2 '"csi2dc":0[fmt:SRGGB10_1X10/640x480]'
+/usr/bin/media-ctl -d /dev/media0 --set-v4l2 '"microchip_isc_scaler":0[fmt:SRGGB10_1X10/640x480]'
+
+#Load custom PIR driver and check interrupt generation
+/sbin/modprobe pir_sensor
+
+#Run pir_send in background
+
+/root/pir_send &
 
 #GStreamer Pipeline 
-gst-launch-1.0 \
+/usr/bin/gst-launch-1.0 \
             v4l2src device=/dev/video0 \
                 ! video/x-raw,width=640,height=480,format=RGB16 \
                     ! videoconvert \
@@ -16,5 +26,4 @@ gst-launch-1.0 \
                             ! rtph264pay pt=96 \
                                 ! udpsink host=192.168.10.22 port=5000
 
-#Load custom PIR driver and check interrupt generation
-modprobe pir_sensor
+
