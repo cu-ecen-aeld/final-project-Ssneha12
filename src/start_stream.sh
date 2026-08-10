252a992b24e0e62e203178738ea84df984c721f7
Administrator <administrator@che-lt-i41644lx.microchip.com>
2026-08-10 14:38:30 +0530

    Added MIPI CSI drivers and Custom PIR driver

---
+++ src/start_stream.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#Configuring Media controller pipeline
+  
+media-ctl -d /dev/media0 --set-v4l2 '"imx219 1-0010":0[fmt:SRGGB10_1X10/640x480]'
+media-ctl -d /dev/media0 --set-v4l2 '"dw-csi.0":0[fmt:SRGGB10_1X10/640x480]'
+media-ctl -d /dev/media0 --set-v4l2 '"csi2dc":0[fmt:SRGGB10_1X10/640x480]'
+media-ctl -d /dev/media0 --set-v4l2 '"microchip_isc_scaler":0[fmt:SRGGB10_1X10/640x480]'
+
+#Load custom PIR driver and check interrupt generation
+modprobe pir_sensor
